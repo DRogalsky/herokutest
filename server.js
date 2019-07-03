a1fff2e861ddcd22f807ee83a96b82e953e66e64
dnl092 <drogalsky14@my.whitworth.edu>
2019-07-03 11:09:55 -0700

    finished initial set up

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+var app = express();
+
+var PORT = 3000;
+
+app.get('/', function (req, res) {
+    res.send("Hello hal")
+})
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
